0aa4c834cb76ff4846fff0ff0ab748db48c8a0de
Collin Schneide <collindanielschneide@gmail.com>
2024-03-24 16:58:00 -0500

    Fix assets, initial ads startup

--- App.tsx
+++ App.tsx
@@ -2,9 +2,21 @@ import React, {useEffect} from 'react';
 import {Platform, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './src/navigation/index';
+import {BannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
+import mobileAds from 'react-native-google-mobile-ads';
 
 function App(): React.JSX.Element {
-  // Set transparent status bar color
+  const unitId =
+    Platform.OS === 'android'
+      ? 'ca-app-pub-9341306803073768/4726842716'
+      : 'ca-app-pub-9341306803073768/4778813606';
+
+  mobileAds()
+    .initialize()
+    .then(adapterStatuses => {
+      console.log(adapterStatuses, 'adapter statuses here');
+    });
+
   const setStatusBarColor = () => {
     if (Platform.OS === 'android') {
       StatusBar.setBackgroundColor('transparent');
@@ -21,6 +33,16 @@ function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <MainStack />
+      <BannerAd
+        size={BannerAdSize.BANNER}
+        unitId={unitId}
+        onAdLoaded={() => {
+          console.log('Advert loaded');
+        }}
+        onAdFailedToLoad={error => {
+          console.error('Advert failed to load: ', error);
+        }}
+      />
     </NavigationContainer>
   );
 }
---
+++ app.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'react-native-google-mobile-ads': {
+    android_app_id: 'ca-app-pub-9341306803073768~9370447696',
+    ios_app_id: 'ca-app-pub-9341306803073768~7247665472',
+  },
+};
